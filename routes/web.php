76365490c3fe46226947eb5b3442fb89a5471f9f
kohei-mana <kohei.manabe@tokyo.supersoftware.co.jp>
2023-05-29 14:32:42 +0900

    新規ユーザー登録で使うルート定義のスケルトンを作成した

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 未ログイン時の出品商品一覧画面(メインページ)のルート
 Route::get('/', function () {
     return view('welcome');
+});
+
+//新規ユーザーメール登録画面のルート
+Route::get('/signup/mail', function (){
+
+});
+
+//新規ユーザー情報登録画面のルート
+Route::get('/signup/identification', function () {
+
+});
+
+//新規ユーザー登録情報確認画面のルート
+Route::get('/signup/confirmation', function () {
+});
+
+//新規ユーザー登録完了画面ルート
+Route::get('/signup/complete', function () {
+    
 });
