668c2c6a2f6751ca98285f4168200944e2fdd631
Kohei-mana <kohei.manabe@tokyo.supersoftware.co.jp>
2023-06-07 19:38:27 +0900

    カテゴリ名の取得

--- app/Http/Controllers/ShowProducts.php
+++ app/Http/Controllers/ShowProducts.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 
+
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+// use app/Models/Category.php;
 
 class ShowProducts extends Controller
 {
@@ -13,7 +16,11 @@ class ShowProducts extends Controller
     public function show(): View
     {
 
-        $products = array_reverse(DB::select('select * from products'));
+        $products = DB::table('products')
+            ->select('products.id', 'products.name', 'products.image', 'products.price', 'products.inventory', 'categories.category_name as category_name')
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->get();
+        // $products = array_reverse(DB::select('select * from products'));
 
 
         return view('welcome', compact('products'));
