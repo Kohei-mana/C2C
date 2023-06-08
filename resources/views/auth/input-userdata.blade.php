cc4a9304ada612d2347dac1ffe299689319cc6a3
yugomoro <yugo.moro@tokyo.supersoftware.co.jp>
2023-06-08 10:38:54 +0900

    マイページ画面の作成

--- resources/views/auth/input-userdata.blade.php
+++ resources/views/auth/input-userdata.blade.php
@@ -13,10 +13,7 @@
         <div class="mt-4">
             <x-input-label for="postal_code" :value="__('postal-code')" />
 
-            <x-text-input id="postal_code" class="block mt-1 w-full"
-                            type="text"
-                            name="postal_code"
-                            required autocomplete="name" />
+            <x-text-input id="postal_code" class="block mt-1 w-full" type="text" name="postal_code" required autocomplete="name" />
 
             <x-input-error :messages="$errors->get('postal_code')" class="mt-2" />
         </div>
@@ -25,20 +22,20 @@
         <div class="mt-4">
             <x-input-label for="address" :value="__('address')" />
 
-            <x-text-input id="address" class="block mt-1 w-full"
-                            type="text"
-                            name="address" required autocomplete="postal_code" />
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" required autocomplete="postal_code" />
 
             <x-input-error :messages="$errors->get('address')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-
+            <x-secondary-button onclick="history.back()" class="ml-4">
+                {{ __('BACK') }}
+            </x-secondary-button>
             <x-primary-button class="ml-4">
                 {{ __('Next') }}
             </x-primary-button>
 
         </div>
-        
+
     </form>
 </x-guest-layout>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,11 +1,42 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('マイページ') }}
+            <div align="right">
+                <x-primary-button onclick="location.href='http://localhost/favorite'">{{ __('いいね！一覧') }}</x-primary-button>
+                <x-primary-button onclick="location.href='http://localhost/listing_history'">{{ __('出品履歴') }}</x-primary-button>
+                <x-primary-button onclick="location.href='http://localhost/purchase_history'">{{ __('購入履歴') }}</x-primary-button>
+            </div>
         </h2>
     </x-slot>
 
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="border-2 border-black">
+                <div class="border-b-2 border-black h-8">
+                    <p class='inline-block w-1/4 font-normal h-8 w-30 float-left border-r-2 border-b-2 border-black px-1 py-1 text-center bg-gray-300'>{{ __('Name') }}</p>
+                    <p class=" py-1 text-center">{{ \Auth::user()->name; }}</p>
+                </div>
+
+                <div class="border-b-2 border-black h-8">
+                    <p class='inline-block w-1/4 font-normal h-8 float-left border-r-2 border-b-2 border-black px-1 py-1 text-center bg-gray-300'>{{ __('Email') }}</p>
+                    <p class="py-1 text-center">{{ \Auth::user()->email; }}</p>
+                </div>
+
+                <div class="border-b-2 border-black h-8">
+                    <p class='inline-block w-1/4 font-normal h-8 w-30 float-left border-r-2 border-b-2 border-black px-1 py-1 text-center bg-gray-300'>{{ __('Postal_code') }}</p>
+                    <p class=" py-1 text-center">〒 {{ \Auth::user()->postal_code; }}</p>
+                </div>
+
+                <div class="border-black h-8">
+                    <p class='inline-block w-1/4 font-normal h-8 w-30 float-left border-r-2 border-black px-1 py-1 text-center bg-gray-300'>{{ __('Address') }}</p>
+                    <p class=" py-1 text-center">{{ \Auth::user()->address; }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
@@ -16,6 +47,12 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-address-form')
                 </div>
             </div>
 
