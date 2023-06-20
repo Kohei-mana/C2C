1938e1c703cd148c5084fca196dbe951f5f1a87d
yugomoro <yugo.moro@tokyo.supersoftware.co.jp>
2023-06-20 14:52:58 +0900

    購入関連の画面作成

--- resources/views/input-shipping-address.blade.php
+++ resources/views/input-shipping-address.blade.php
@@ -1,12 +1,18 @@
-<x-guest-layout>
-    <form>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight" align="center">
+            {{ __('送付先入力') }}
+        </h2>
+    </x-slot>
+
+    <form method="POST" action="{{ route('input-payment-information') }}" enctype="multipart/form-data" style="width: 30%;" class="mx-auto">
         @csrf
 
         <!-- Postal-code -->
         <div class="mt-4">
             <x-input-label for="postal_code" :value="__('postal-code')" />
 
-            <x-text-input id="postal_code" class="block mt-1 w-full" type="text" name="postal_code" required autocomplete="name" />
+            <x-text-input id="postal_code" class="block mt-1 w-full" type="text" name="postal_code" required value="{{ Auth::user()->postal_code }}" />
 
             <x-input-error :messages="$errors->get('postal_code')" class="mt-2" />
         </div>
@@ -15,7 +21,7 @@
         <div class="mt-4">
             <x-input-label for="address" :value="__('address')" />
 
-            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" required autocomplete="postal_code" />
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" required value="{{ Auth::user()->address }}" />
 
             <x-input-error :messages="$errors->get('address')" class="mt-2" />
         </div>
@@ -23,14 +29,9 @@
         <!-- Name -->
         <div class="mt-4">
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required value="{{ Auth::user()->name }}" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-
-        <div class="mt-4">
-            <div>お届け予定日</div>
-        </div>
-
 
         <div class="flex items-center justify-end mt-4">
             <x-secondary-button onclick="history.back()" class="ml-4">
@@ -43,4 +44,4 @@
         </div>
 
     </form>
-</x-guest-layout>
+</x-app-layout>
