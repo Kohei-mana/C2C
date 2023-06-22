0e56cfeee071128f76d43586164ff06c352105a1
Kohei-mana <kohei.manabe@tokyo.supersoftware.co.jp>
2023-06-22 13:45:03 +0900

    ルート訂正

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,14 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', [ShowProducts::class, 'show'])
-    ->name('home');
+    Route::get('/', [ShowProducts::class, 'show'])
+        ->name('home');
 
-Route::get('/serched', [ShowProducts::class, 'search'])
-    ->name('search-product');
+    Route::get('/searched', [ShowProducts::class, 'search'])
+        ->name('search-product');
 
-Route::get('/product-detail/{id}', [ShowProducts::class, 'showDetail'])
-    ->name('product-detail');
-
+    Route::get('/product-detail/{id}', [ShowProducts::class, 'showDetail'])
+        ->name('product-detail');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -38,49 +37,55 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/favorite', [FavoriteController::class, 'showFavoriteProducts'])
-        ->name('favorite');
+    Route::get('/favorite', [FavoriteController::class, 'showFavoriteProducts'])->name('favorite');
 
-    Route::get('/listing_history', [ExhibitController::class, 'showHistory'])->name('listing_history');
+    Route::get('/listing_history', [ExhibitController::class, 'showAll'])->name('listing_history');
 
-    Route::get('/exhibition-product/{id}', [ExhibitController::class, 'show'])->name('exhibition-product');
-    Route::post('/stopListing/{id}', [ExhibitController::class, 'stopListing'])->name('stopListing');
-    Route::post('/resumeListing/{id}', [ExhibitController::class, 'resumeListing'])->name('resumeListing');
+    Route::get('/exhibition-product/{id}', [ExhibitController::class, 'showSpecific'])->name('exhibition-product');
+    Route::post('/update-listing/{id}', [ExhibitController::class, 'updateListing'])->name('updateListing');
 
     Route::get('/purchase_history', [PurchaseController::class, 'showHistory'])->name('purchase_history');
 
-    Route::get('/exhibit', [ExhibitController::class, 'exhibitPage'])->name('exhibit');
-    Route::post('/confirm-exhibit', [ExhibitController::class, 'confirmExhibitPage'])->name('confirm-exhibit');
-    Route::get('/complete-exhibit', [ExhibitController::class, 'store'])->name('complete-exhibit');
+    Route::get('/exhibit', [ExhibitController::class, 'create'])->name('exhibit');
+    Route::post('/confirm-exhibit', [ExhibitController::class, 'confirm'])->name('confirm-exhibit');
+    Route::post('/complete-exhibit', [ExhibitController::class, 'store'])->name('complete-exhibit');
 
+    Route::get('/product-detail/{id}', [ShowProducts::class, 'showDetail'])
+        ->name('product-detail');
 
     // // いいね
-    Route::get('/product-detail/favorite/{product}', [FavoriteController::class, 'makeFavorite'])->name('addfavorite');
-    Route::get('/product-detail/notfavorite/{product}', [FavoriteController::class, 'removeFavorite'])->name('notfavorite');
+    // Route::get('/product-detail/favorite/{product}', [FavoriteController::class, 'makeFavorite'])->name('addfavorite');
+    Route::post('/product-detail/favorite/{product}', [FavoriteController::class, 'makeFavorite'])->name('addfavorite');
+    Route::post('/product-detail/notfavorite/{product}', [FavoriteController::class, 'removeFavorite'])->name('notfavorite');
 
     //カートに追加
-    Route::get('/product-detail//{product}', [PurchaseController::class, 'addToCart'])
-    ->name('add_to_cart');
+
+    Route::post('/product-detail//{product}', [PurchaseController::class, 'addToCart'])
+
+        ->name('add_to_cart');
 
     //カートに移動
     Route::get('/shopping_cart', [PurchaseController::class, 'shoppingCartPage'])
-    ->name('shopping_cart');
+        ->name('shopping_cart');
     //カートから削除
-    Route::get('/shopping_cart/deleted', [PurchaseController::class, 'removeFromCart'])
-    ->name('remove_from_cart');
+
+    Route::post('/shopping_cart/deleted', [PurchaseController::class, 'removeFromCart'])
+        ->name('remove_from_cart');
 
 
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['verified'])->name('dashboard');
 
-
     Route::get('/input-shipping-address', [PurchaseController::class, 'inputShippingAddress'])->name('input-shipping-address');
-
+    Route::post('/input-payment-information', [PurchaseController::class, 'inputPaymentInformation'])->name('input-payment-information');
+    Route::post('/complete-purchase', [ExhibitController::class, 'store'])->name('complete-purchase');
 });
+
 
 require __DIR__ . '/auth.php';
